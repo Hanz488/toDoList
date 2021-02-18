fc61ae78359b9f8dc50b968c906ca8c28d5069f0
Hanz488 <ahanzch@gmail.com>
2021-02-18 23:28:46 +0900

    usingVue

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
-
-function addGoal() {
-    const inputValue = inputEl.value;
-    const listItemEl = document.createElement('li');
-    listItemEl.textContent = inputValue;
-    listEl.appendChild(listItemEl);
-    inputEl.value= '';
-}
-
-buttonEl.addEventListener('click', addGoal);
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            inputValue: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.inputValue);
+            this.inputValue = '';
+        }
+    }
+}).mount('#app');
